d4bac7f820a8489fab2d636761f8a3e882aeda3a
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2024-11-17 09:45:31 +0600

    add project

---
+++ assets/js/main.js
@@ -0,0 +1,155 @@
+/***************************************************
+==================== JS INDEX ======================
+****************************************************
+
+// sticky header 
+// Smooth active
+// Preloader
+// Side Info Js
+// meanmenu activation 
+// Register GSAP Plugins
+// Counter active
+// testimonial slider
+// wow js activation 
+
+
+****************************************************/
+
+(function ($) {
+  "use strict";
+
+
+  // sticky header 
+  let header = document.querySelector('.header-sticky');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+      header.classList.add('sticky')
+    } else {
+      header.classList.remove('sticky')
+    }
+  })
+
+
+
+  // Smooth active
+  var device_width = window.screen.width;
+
+  if (device_width > 767) {
+    if (document.querySelector("#has_smooth").classList.contains("has-smooth")) {
+      const smoother = ScrollSmoother.create({
+        smooth: 0.5,
+        effects: device_width < 1025 ? false : true,
+        smoothTouch: 0.1,
+        normalizeScroll: false,
+        ignoreMobileResize: true,
+      });
+    }
+
+  }
+
+
+  // Preloader
+  $(document).ready(function () {
+    $('#container').addClass('loaded');
+    if ($('#container').hasClass('loaded')) {
+      $('#preloader').delay(1000).queue(function () {
+        $(this).remove();
+      });
+    }
+  });
+
+
+  // Side Info Js
+  $(".side-info-close,.offcanvas-overlay").on("click", function () {
+    $(".side-info").removeClass("info-open");
+    $(".offcanvas-overlay").removeClass("overlay-open");
+  });
+  $(".side-toggle").on("click", function () {
+    $(".side-info").addClass("info-open");
+    $(".offcanvas-overlay").addClass("overlay-open");
+  });
+
+
+  // meanmenu activation 
+  $('.main-menu').meanmenu({
+    meanScreenWidth: "1199",
+    meanMenuContainer: '.mobile-menu',
+    meanMenuCloseSize: '28px',
+  });
+
+
+  // Register GSAP Plugins
+  gsap.registerPlugin(ScrollTrigger);
+
+
+
+  // Counter active
+  if ('counterUp' in window) {
+    const skill_counter = window.counterUp.default
+    const skill_cb = entries => {
+
+      entries.forEach(entry => {
+        const el = entry.target
+        if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+          skill_counter(el, {
+            duration: 1500,
+            delay: 16,
+          })
+          el.classList.add('is-visible')
+        }
+      })
+    }
+
+    const IO = new IntersectionObserver(skill_cb, {
+      threshold: 1
+    })
+
+    const els = document.querySelectorAll('.t-counter');
+    els.forEach((el) => {
+      IO.observe(el)
+    });
+  }
+
+
+  // testimonial slider
+  if (('.testimonial-slider').length) {
+    var testimonial_slider = new Swiper(".testimonial-slider", {
+      loop: false,
+      slidesPerView: 1,
+      spaceBetween: 100,
+      speed: 1800,
+      watchSlidesProgress: true,
+      navigation: {
+        prevEl: ".testimonial-button-prev",
+        nextEl: ".testimonial-button-next",
+      },
+      pagination: {
+        el: '.testimonial-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        // when window width is >= px
+        576: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 1,
+        },
+        992: {
+          slidesPerView: 1,
+        },
+        1201: {
+          slidesPerView: 1,
+        },
+        1367: {
+          slidesPerView: 1,
+        },
+      }
+    });
+  }
+
+  // wow js activation 
+  new WOW().init();
+
+})(jQuery);

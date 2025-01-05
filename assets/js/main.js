81ec0f318a8fcbcd2e7f835ef016bb8c4ff5b3d6
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-01-05 17:28:23 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -511,18 +511,25 @@
     });
   });
 
+  // moving testimonial 
+  if (document.querySelectorAll(".moving-testimonial").length > 0) {
+    mm.add("(min-width: 768px)", () => {
+      const e = document.querySelector(".moving-testimonial"),
+        t = e.querySelector(".pin"),
+        o = e.querySelectorAll(".card");
+      ScrollTrigger.create({ trigger: t, start: "top top", end: "bottom bottom", pin: !0, pinSpacing: !1, scrub: !0 }), gsap.set(o, { yPercent: 50, y: 0.5 * window.innerHeight + 1 });
+      const n = gsap.timeline({ paused: !0, scrollTrigger: { trigger: e, start: "top top", end: "bottom bottom", scrub: !0 } });
+      n.to(o, { yPercent: -50, y: -0.5 * window.innerHeight, duration: 1, stagger: -0.12, ease: CustomEase.create("custom", "M0,0 C0,0 0.098,0.613 0.5,0.5 0.899,0.386 1,1 1,1") }, "sameStep"),
+        n.to(o, { rotation: () => 20 * (Math.random() - 0.5), stagger: -0.12, duration: 0.5, ease: "power3.out" }, "sameStep"),
+        n.to(o, { rotation: 0, stagger: -0.12, duration: 0.5, ease: "power3.in" }, "sameStep+=0.5");
+    });
+  }
 
-  mm.add("(min-width: 768px)", () => {
-    const e = document.querySelector(".moving-testimonial"),
-      t = e.querySelector(".pin"),
-      o = e.querySelectorAll(".card");
-    ScrollTrigger.create({ trigger: t, start: "top top", end: "bottom bottom", pin: !0, pinSpacing: !1, scrub: !0 }), gsap.set(o, { yPercent: 50, y: 0.5 * window.innerHeight + 1 });
-    const n = gsap.timeline({ paused: !0, scrollTrigger: { trigger: e, start: "top top", end: "bottom bottom", scrub: !0 } });
-    n.to(o, { yPercent: -50, y: -0.5 * window.innerHeight, duration: 1, stagger: -0.12, ease: CustomEase.create("custom", "M0,0 C0,0 0.098,0.613 0.5,0.5 0.899,0.386 1,1 1,1") }, "sameStep"),
-      n.to(o, { rotation: () => 20 * (Math.random() - 0.5), stagger: -0.12, duration: 0.5, ease: "power3.out" }, "sameStep"),
-      n.to(o, { rotation: 0, stagger: -0.12, duration: 0.5, ease: "power3.in" }, "sameStep+=0.5");
-  });
-
-
+  // capability hover active 
+  if (document.querySelectorAll(".capability-hover-active").length > 0) {
+    $('.capability-hover-active .capability-box').on("mouseover", function () {
+      $(this).addClass('active').siblings().removeClass('active');
+    });
+  }
 
 })(jQuery);

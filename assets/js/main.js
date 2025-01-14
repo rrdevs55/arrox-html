d39e31ff4af5289469192e001b208df418c923ab
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-01-14 10:57:23 +0600

    restore js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1004,6 +1004,329 @@
   }
 
 
+  // Animation Word
+  let animation_word_anim_items = document.querySelectorAll(".word-anim");
+
+  animation_word_anim_items.forEach((word_anim_item) => {
+
+    var stagger_value = 0.04
+    var translateX_value = false
+    var translateY_value = false
+    var onscroll_value = 1
+    var data_delay = 0.1
+    var data_duration = 0.75
+
+    if (word_anim_item.getAttribute("data-stagger")) {
+      stagger_value = word_anim_item.getAttribute("data-stagger");
+    }
+    if (word_anim_item.getAttribute("data-translateX")) {
+      translateX_value = word_anim_item.getAttribute("data-translateX");
+    }
+
+    if (word_anim_item.getAttribute("data-translateY")) {
+      translateY_value = word_anim_item.getAttribute("data-translateY");
+    }
+
+    if (word_anim_item.getAttribute("data-on-scroll")) {
+      onscroll_value = word_anim_item.getAttribute("data-on-scroll");
+    }
+    if (word_anim_item.getAttribute("data-delay")) {
+      data_delay = word_anim_item.getAttribute("data-delay");
+    }
+    if (word_anim_item.getAttribute("data-duration")) {
+      data_duration = word_anim_item.getAttribute("data-duration");
+    }
+
+    if (onscroll_value == 1) {
+      if (translateX_value && !translateY_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: data_duration,
+          x: translateX_value,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          delay: data_delay,
+          scrollTrigger: {
+            trigger: word_anim_item,
+            start: 'top 90%'
+          }
+        });
+      }
+
+      if (translateY_value && !translateX_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          y: translateY_value,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          scrollTrigger: {
+            trigger: word_anim_item,
+            start: 'top 90%'
+          }
+        });
+      }
+
+      if (translateY_value && translateX_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          x: translateX_value,
+          y: translateY_value,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          scrollTrigger: {
+            trigger: word_anim_item,
+            start: 'top 90%'
+          }
+        });
+      }
+
+      if (!translateX_value && !translateY_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          x: 20,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          scrollTrigger: {
+            trigger: word_anim_item,
+            start: 'top 85%',
+          }
+        });
+      }
+    } else {
+      if (translateX_value > 0 && !translateY_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          x: translateX_value,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+
+      if (translateY_value > 0 && !translateX_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          y: translateY_value,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+
+      if (translateY_value > 0 && translateX_value > 0) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          x: translateX_value,
+          y: translateY_value,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+
+      if (!translateX_value && !translateY_value) {
+        let split_word = new SplitText(word_anim_item, {
+          type: "chars, words"
+        })
+        gsap.from(split_word.words, {
+          duration: 1,
+          delay: data_delay,
+          x: 20,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+
+    }
+
+  });
+
+  // Full Character Setup 
+  var animation_char_come_items = document.querySelectorAll(".char-anim")
+  animation_char_come_items.forEach((item) => {
+
+    var stagger_value = 0.05
+    var translateX_value = 20
+    var translateY_value = false
+    var onscroll_value = 1
+    var data_delay = 0.1
+    var data_duration = 1
+    var ease_value = "power2.out"
+
+    if (item.getAttribute("data-stagger")) {
+      stagger_value = item.getAttribute("data-stagger");
+    }
+    if (item.getAttribute("data-translateX")) {
+      translateX_value = item.getAttribute("data-translateX");
+    }
+    if (item.getAttribute("data-translateY")) {
+      translateY_value = item.getAttribute("data-translateY");
+    }
+    if (item.getAttribute("data-on-scroll")) {
+      onscroll_value = item.getAttribute("data-on-scroll");
+    }
+    if (item.getAttribute("data-delay")) {
+      data_delay = item.getAttribute("data-delay");
+    }
+    if (item.getAttribute("data-ease")) {
+      ease_value = item.getAttribute("data-ease");
+    }
+    if (item.getAttribute("data-duration")) {
+      data_duration = item.getAttribute("data-duration");
+    }
+
+    if (onscroll_value == 1) {
+      if (translateX_value > 0 && !translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: data_duration,
+          delay: data_delay,
+          x: translateX_value,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          ease: ease_value,
+          scrollTrigger: {
+            trigger: item,
+            start: 'top 85%',
+          }
+        });
+      }
+      if (translateY_value > 0 && !translateX_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: data_duration,
+          delay: data_delay,
+          y: translateY_value,
+          autoAlpha: 0,
+          ease: ease_value,
+          stagger: stagger_value,
+          scrollTrigger: {
+            trigger: item,
+            start: 'top 85%',
+          }
+        });
+      }
+      if (translateX_value && translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 2,
+          delay: data_delay,
+          y: translateY_value,
+          x: translateX_value,
+          autoAlpha: 0,
+          ease: ease_value,
+          stagger: stagger_value,
+          scrollTrigger: {
+            trigger: item,
+            start: 'top 85%',
+          }
+        });
+      }
+      if (!translateX_value && !translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 1,
+          delay: data_delay,
+          x: 50,
+          autoAlpha: 0,
+          stagger: stagger_value,
+          ease: ease_value,
+          scrollTrigger: {
+            trigger: item,
+            start: 'top 85%',
+          }
+        });
+      }
+    } else {
+      if (translateX_value > 0 && !translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 1,
+          delay: data_delay,
+          x: translateX_value,
+          ease: ease_value,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+      if (translateY_value > 0 && !translateX_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 1,
+          delay: data_delay,
+          y: translateY_value,
+          autoAlpha: 0,
+          ease: ease_value,
+          stagger: stagger_value
+        });
+      }
+      if (translateX_value && translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 1,
+          delay: data_delay,
+          y: translateY_value,
+          x: translateX_value,
+          ease: ease_value,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+      if (!translateX_value && !translateY_value) {
+        let split_char = new SplitText(item, {
+          type: "chars, words"
+        });
+        gsap.from(split_char.chars, {
+          duration: 1,
+          delay: data_delay,
+          ease: ease_value,
+          x: 50,
+          autoAlpha: 0,
+          stagger: stagger_value
+        });
+      }
+    }
+
+  });
+
+
   let revealContainers = document.querySelectorAll(".return");
 
   revealContainers.forEach((container) => {

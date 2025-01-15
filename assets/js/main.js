2b9a400a5337bd2a701311d18cb52ce5e37ce8ed
rana <rana.rrdevs@gmail.com>
2025-01-15 12:12:51 +0600

    done hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1300,4 +1300,27 @@
     });
   });
 
+
+
+  var path = `M 10 100 Q 500 100 990 100`
+  var finalPath = `M 10 100 Q 500 100 990 100`
+
+  var string = document.querySelector("#string")
+  string.addEventListener("mousemove", function (dets) {
+    path = `M 10 100 Q ${dets.x}  ${dets.y} 990 100`
+    gsap.to("svg path", {
+      attr: { d: path },
+      duration: 0.5,
+      ease: "power3.out",
+    })
+  });
+
+  string.addEventListener("mouseleave", function (dets) {
+    gsap.to("svg path", {
+      attr: { d: finalPath },
+      duration: 0.6,
+      ease: "elastic",
+    })
+  });
+
 })(jQuery);

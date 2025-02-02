3df6cd425d776285bd642c8ffaf01d68cbc4c8a1
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2025-02-02 17:22:47 +0600

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,6 +127,32 @@
     });
   }
 
+
+  // Image Reveal Animation
+  let img_anim_reveal = document.querySelectorAll(".img_anim_reveal");
+
+  img_anim_reveal.forEach((img_reveal) => {
+    let image = img_reveal.querySelector("img");
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: img_reveal,
+        start: "top 50%",
+      }
+    });
+
+    tl.set(img_reveal, { autoAlpha: 1 });
+    tl.from(img_reveal, 1.5, {
+      yPercent: -100,
+      ease: Power2.out
+    });
+    tl.from(image, 1.5, {
+      yPercent: 100,
+      scale: 1.3,
+      delay: -1.5,
+      ease: Power2.out
+    });
+  });
+
   // testimonial slider
   if (('.testimonial-slider').length) {
     var testimonial_slider = new Swiper(".testimonial-slider", {

fc20dc0af9711c7e2a8573260382a663804377b1
Md Jihad Hossan <jihad.marine@gmail.com>
2025-02-16 14:38:46 +0600

    servise details page animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1298,6 +1298,27 @@ Data Css js
     });
   }
 
+  // approach-area
+  // ====================================================
+  if (document.querySelectorAll(".approach-area-service-details-page").length > 0) {
+
+    const boxes = document.querySelectorAll(".approach-box");
+
+    gsap.from(boxes, {
+      x: "100%",
+      duration: 1,
+      stagger: 0.3,
+      ease: "power2.out",
+      scrollTrigger: {
+        scrub: 2,
+        trigger: ".approach-wrapper-box",
+        start: "top 100%",
+        end: "bottom 40%",
+        toggleActions: "play none none reverse",
+      }
+    });
+  }
+
 
   // button animation
   // ====================================================
